21d87b793b712f7863b3a27acdd6a3163fea02a1
Gabriele Luz <gabrieleluz@infojr.com.br>
2019-08-05 19:19:46 -0300

    updates database & route

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,12 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-      factory(App\User::class, 30)->create();
+        DB::table('users')->insert([
+            'name' => "Admin",
+            'email' => 'admin@email.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+        factory(App\User::class, 30)->create();
     }
 }
